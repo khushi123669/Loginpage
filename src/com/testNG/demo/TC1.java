1265dc3eb1d247e09c6910ba7f178bbb9d557536
Khushboo <khushboogoel707@gmail.com>
2025-06-18 11:15:00 -0500

    successful

---
+++ src/com/testNG/demo/TC1.java
@@ -0,0 +1,23 @@
+package com.testNG.demo;
+
+import org.testng.annotations.Test;
+
+public class TC1 {
+	
+     @Test
+	public void testcase1()
+	{
+		System.out.println("welcome to testNG");
+		
+	}
+	
+     
+	
+	
+	
+	
+	
+	
+	
+	
+}
